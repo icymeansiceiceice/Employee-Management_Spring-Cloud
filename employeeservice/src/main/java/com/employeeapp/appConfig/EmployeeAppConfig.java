0f7eab17e8c388c9591ba80bf5254b4da58e7fcf
nOttOD4y <kaungmyat97k@gmail.com>
2024-01-20 00:58:35 +0630

    testing async

--- employeeservice/src/main/java/com/employeeapp/appConfig/EmployeeAppConfig.java
+++ employeeservice/src/main/java/com/employeeapp/appConfig/EmployeeAppConfig.java
@@ -1,20 +1,27 @@
 package com.employeeapp.appConfig;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class EmployeeAppConfig {
     
+    @Value("${addressservice.base.url}") 
+    private String addressBaseUrl;
+
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
 
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public WebClient webClient(){
+        return WebClient.builder().baseUrl(addressBaseUrl).build();
     }
+
+
 }
--- employeeservice/src/main/java/com/employeeapp/services/EmployeeService.java
+++ employeeservice/src/main/java/com/employeeapp/services/EmployeeService.java
@@ -2,8 +2,11 @@ package com.employeeapp.services;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.employeeapp.entity.Employee;
 import com.employeeapp.repo.EmployeeRepo;
@@ -20,14 +23,32 @@ public class EmployeeService {
     private ModelMapper modelMapper;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WebClient webClient;
+
+    //private RestTemplate restTemplate;
+
+    // public EmployeeService(RestTemplateBuilder restTemplateBuilder,@Value("${addressservice.base.url}") String addressBaseUrl){
+    //     this.restTemplate = restTemplateBuilder.rootUri(addressBaseUrl).build();
+    // }
+
 
 
     public EmployeeResponse getEmployeeByID(int id){
         Employee employee = employeeRepo.findById(id).get();
         EmployeeResponse res = modelMapper.map(employee, EmployeeResponse.class);
-        AddressResponse addressResponse = restTemplate.getForObject("http://localhost:8080/address/{id}", AddressResponse.class, id);
+        AddressResponse addressResponse = callingAddressResponseUsingWebClient(id);
         res.setAddressResponse(addressResponse);
         return res;
     }
+
+    //sync
+    // private AddressResponse callingAddressResponseUsingRestTemplate(int id){
+    //     return restTemplate.getForObject("/address/{id}", AddressResponse.class, id);
+    // }
+    
+    //async
+    private AddressResponse callingAddressResponseUsingWebClient(int id){
+        return webClient.get().uri("/address/"+id).retrieve().bodyToMono(AddressResponse.class).block();
+    }
+
 }
